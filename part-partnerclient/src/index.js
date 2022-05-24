f333678383ef7d4551fb968a1b105d79b5b64b4c
Klay Thacker <klaythacker11@gmail.com>
2022-05-23 19:41:58 -0500

    adding firebase configs

--- part-partnerclient/src/index.js
+++ part-partnerclient/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import firebase from 'firebase/compat/app';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_API_KEY,
+};
+firebase.initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
