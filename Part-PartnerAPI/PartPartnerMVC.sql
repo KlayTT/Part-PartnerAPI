6a34352dadcf364e45d77e4cb9165fd043e0efc5
Klay Thacker <klaythacker11@gmail.com>
2022-05-24 19:39:17 -0500

    Commit Firebase and mvc changes

--- Part-PartnerAPI/PartPartnerMVC.sql
+++ Part-PartnerAPI/PartPartnerMVC.sql
@@ -13,7 +13,7 @@ USE PartPartnerMVC
 GO
 
 DROP TABLE IF EXISTS Users;
-DROP TABLE IF EXISTS Car;
+DROP TABLE IF EXISTS Cars;
 DROP TABLE IF EXISTS Part;
 
 CREATE TABLE Users (
@@ -32,10 +32,11 @@ CREATE TABLE Cars (
   Color VARCHAR (255) NOT NUll,
   ImageURL VARCHAR (255) NOT NUll,
   PartId VARCHAR(255),
+  [Uid] VARCHAR(255) NOT NULL,
 
 );
 
-CREATE TABLE Part (
+CREATE TABLE Parts (
   Id INT NOT NULL PRIMARY KEY IDENTITY,
   [Name] VARCHAR (255) NOT NUll,
   Price DECIMAL(17, 2) NOT NULL,
@@ -43,16 +44,19 @@ CREATE TABLE Part (
   Miles VARCHAR (255),
   DatePurchased DATETIME,
   NextMatnience DATETIME,
+  [Uid] VARCHAR(255) NOT NULL,
 
 );
 
-INSERT INTO Users (UserName, Email, FirebaseUserId) VALUES ('KlayTT', 'klaythacker11@gmail.com','AVukDXArGdfEYdDn6vbmpJ99Hbu2');
-INSERT INTO Users (UserName, Email, FirebaseUserId) VALUES ('Tru G', 'klaythackerworkmail@gmail.com','AVukDXArGdfEYdDn6vbmpJ99Hbu2');
+INSERT INTO Users (UserName, Email, FirebaseUserId) VALUES ('KlayTT', 'klaythacker11@gmail.com','1WEZNgZ96BZf5yfHWeSFM84A0N03');
+INSERT INTO Users (UserName, Email, FirebaseUserId) VALUES ('Tru G', 'klaythackerworkmail@gmail.com','Sy0CbC9WcJSslguFZAkMa4fX2Af2');
 
-INSERT INTO Cars ([Name], [Year], Color, ImageURL) VALUES ('VW GTI Custom', 'Navy Blue', '2013', 'https://i.pinimg.com/originals/88/95/6e/88956ebf9277bf940401b3e0b370325b.jpg');
-INSERT INTO Cars ([Name], [Year], Color, ImageURL) VALUES ('Porsche 911 GT3 RS', 'Black', '2019' ,'https://cdn1.mecum.com/auctions/ca0821/ca0821-465261/images/1-1621285098983.jpg?1628545960000');
-INSERT INTO Cars ([Name], [Year], Color, ImageURL) VALUES ('Toyota Previa aka the Space Ship', 'Silver', '1991' ,'https://consumerguide.com/wp-content/uploads/2014/07/93812141990615.jpg');
-INSERT INTO Cars ([Name], [Year], Color, ImageURL) VALUES ('Nissan 300ZX', 'Red', '1992' ,'https://bringatrailer.com/wp-content/uploads/2019/05/1992_nissan_300zx_twin_turbo_1559157297ff9f98764daNissan-300ZXTT-8.jpg?fit=940%2C623');
+INSERT INTO Cars ([Name], [Year], Color, ImageURL, [Uid]) VALUES ('VW GTI Custom', 'Navy Blue', '2013', 'https://i.pinimg.com/originals/88/95/6e/88956ebf9277bf940401b3e0b370325b.jpg','1WEZNgZ96BZf5yfHWeSFM84A0N03');
+INSERT INTO Cars ([Name], [Year], Color, ImageURL, [Uid]) VALUES ('Porsche 911 GT3 RS', 'Black', '2019' ,'https://cdn1.mecum.com/auctions/ca0821/ca0821-465261/images/1-1621285098983.jpg?1628545960000','1WEZNgZ96BZf5yfHWeSFM84A0N03');
+INSERT INTO Cars ([Name], [Year], Color, ImageURL, [Uid]) VALUES ('Toyota Previa aka the Space Ship', 'Silver', '1991' ,'https://consumerguide.com/wp-content/uploads/2014/07/93812141990615.jpg','Sy0CbC9WcJSslguFZAkMa4fX2Af2');
+INSERT INTO Cars ([Name], [Year], Color, ImageURL, [Uid]) VALUES ('Nissan 300ZX', 'Red', '1992' ,'https://bringatrailer.com/wp-content/uploads/2019/05/1992_nissan_300zx_twin_turbo_1559157297ff9f98764daNissan-300ZXTT-8.jpg?fit=940%2C623','Sy0CbC9WcJSslguFZAkMa4fX2Af2');
 
-INSERT INTO Part ([Name], Price, ImageURL, Miles) VALUES ('Duralast Starter 19442', $119.99, 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSWGl0FCEd41XWr68IM23OF9Fme9d2V_dnO743sY4l_gGil_fM4PR-xP5KrICyKqEUiPnDhN3AoyA&usqp=CAc', '155k');
+INSERT INTO Parts ([Name], Price, ImageURL, Miles, [Uid]) VALUES ('Duralast Starter 19442', $119.99, 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSWGl0FCEd41XWr68IM23OF9Fme9d2V_dnO743sY4l_gGil_fM4PR-xP5KrICyKqEUiPnDhN3AoyA&usqp=CAc', '155k','1WEZNgZ96BZf5yfHWeSFM84A0N03');
+
+INSERT INTO Parts ([Name], Price, ImageURL, Miles, [Uid]) VALUES ('Duralast Starter 19443', $119.99, 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSWGl0FCEd41XWr68IM23OF9Fme9d2V_dnO743sY4l_gGil_fM4PR-xP5KrICyKqEUiPnDhN3AoyA&usqp=CAc', '155k','Sy0CbC9WcJSslguFZAkMa4fX2Af2');
 
--- Part-PartnerAPI/Program.cs
+++ Part-PartnerAPI/Program.cs
@@ -1,6 +1,38 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+var firebaseProjectId = builder.Configuration.GetValue<string>("FirebaseProjectId");
+var googleTokenUrl = $"https://securetoken.google.com/{firebaseProjectId}";
+
 // Add services to the container.
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("https://localho.st:7297", "https://localho.st:5297").AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin();
+                      });
+});
+
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = googleTokenUrl;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = googleTokenUrl,
+            ValidateAudience = true,
+            ValidAudience = firebaseProjectId,
+            ValidateLifetime = true
+        };
+    });
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -17,6 +49,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(builder => { builder.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin(); });
 
 app.UseAuthorization();
 
