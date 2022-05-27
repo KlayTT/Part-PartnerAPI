6af2a5e593d10e50283076b62a14f4e2ae26481b
Klay Thacker <klaythacker11@gmail.com>
2022-05-27 15:05:54 -0500

    MOdels

---
+++ Part-PartnerAPI/Models/Parts.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace Part_PartnerAPI.Models
+{
+    public class Parts
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        [ValidateNever]
+        public string Miles { get; set; }
+        [ValidateNever]
+        public DateTime DatePurchased { get; set; }
+        [ValidateNever]
+        public DateTime NextMatnience { get; set; }
+        public string Uid { get; set; }
+
+    }
+}
