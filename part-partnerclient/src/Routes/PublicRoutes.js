7cb979fb55d303e9db42dec6fbba6ce77d08d762
Klay Thacker <klaythacker11@gmail.com>
2022-06-04 14:10:54 -0500

    Register view and route

--- part-partnerclient/src/Routes/PublicRoutes.js
+++ part-partnerclient/src/Routes/PublicRoutes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from '../Views/Home';
 import Login from '../Views/Login';
+import Register from '../Views/Register';
 
 export default function PublicRoutes({ isLoggedIn }) {
     return (
         <Routes>
             <Route exact path="/" element={isLoggedIn ? <Home /> : <Login />} />
+            <Route exact path="/register" element={isLoggedIn ? <Home /> : <Register />} />
         </Routes>
   );
 }
