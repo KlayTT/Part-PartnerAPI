e49c89255c684c35477def86fc4e36084f81035c
Klay Thacker <klaythacker11@gmail.com>
2022-06-04 09:35:45 -0500

    Added styles before new branch

--- part-partnerclient/src/index.js
+++ part-partnerclient/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-// import './styles/styles.scss';
+import './Styles/styles.scss';
 import firebase from 'firebase/compat/app';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
